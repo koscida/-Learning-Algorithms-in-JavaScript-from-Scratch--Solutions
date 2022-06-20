c418d7a466838c2bd35e84dff5082291f19c1c67
Brittany Ann <britkos@gmail.com>
2022-06-20 13:49:54 +0000

    isPalindrome

--- JSAlgorithms/IsPalindrome.js
+++ JSAlgorithms/IsPalindrome.js
@@ -10,7 +10,6 @@ function isPalindrome(text) {
   let pal = true;
   // O(n) + O(n)
   const t = [...(text.toLowerCase().replace(/\W/g, ''))]
-  console.log(t)
   // O(n/2)
   for (i of [...Array(Math.floor(t.length/2)).keys()]) {
     // O(1)  // O(1)
@@ -29,12 +28,16 @@ function isPalindrome1(text) {
 
 
 
-const text1 = "aaa bbb aaa"
-const text2 = "aaa bbv aaa"
+const text1 = "race car"
+const text2 = "Madam, I'm Adam"
+const text3 = "fdjskalg ewkjals"
 
 console.log(isPalindrome(text1))
 console.log(isPalindrome(text2))
+console.log(isPalindrome(text3))
 
 // console.log(isPalindrome1(text1))
 // console.log(isPalindrome1(text2))
+// console.log(isPalindrome1(text3))
+
 
